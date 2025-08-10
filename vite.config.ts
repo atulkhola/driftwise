6e1a938c3f50bea7159794fc8358e13c9dfa4e23
Atul Khola <atulkhola@Atuls-MacBook-Pro.local>
2025-08-10 14:56:43 +0530

    feat: minimal Vite app + dynamic base

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,4 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 const repo = (process.env.GITHUB_REPOSITORY || "").split("/")[1] || "";
 const base = repo ? `/${repo}/` : "/";
-export default defineConfig({ plugins: [react()], base });
+export default defineConfig({ plugins:[react()], base });
